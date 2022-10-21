4878a415ab2b11a70b557e419375cf18aad05d60
Harrison <jiksdevops@gmail.com>
2022-10-21 14:48:19 +0200

    Updates blog contents

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const articles = [
 		title: 'How I jumped into react: lessons learned',
 		date: new Date(2022, 9, 4),
 		length: 11,
-		snippet: `Come waste your time with me`,
+		snippet: `Very easy to over engineer things`,
 	},
 	{
 		id: 2,
@@ -15,21 +15,21 @@ const articles = [
 	},
 	{
 		id: 3,
-		title: 'Redux toolkit and ContextApi: My view',
+		title: 'Redux toolkit and ContextAPI: My view',
 		date: new Date(2021, 7, 11),
 		length: 5,
 		snippet: `A personal reflection.`,
 	},
 	{
 		id: 4,
-		title: 'Too many frameworks: the good and ugly',
+		title: 'Too many frameworks: the good and the ugly',
 		date: new Date(2021, 5, 4),
 		length: 5,
 		snippet: `Decision paralysis`,
 	},
 	{
 		id: 5,
-		title: 'Beginners and the Doc',
+		title: 'Beginners and the Docs',
 		date: new Date(2021, 6, 4),
 		length: 5,
 		snippet: `Verbose intermediating lengthy text`,
